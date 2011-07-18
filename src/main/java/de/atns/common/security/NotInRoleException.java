904a9f30dddf87b6c22530df2147ad9c9d200b24
Michael Wolter <michael.wolter@atns.de>
2011-07-18 17:11:35 +0200

    NotInRoleException gwt serializeable

--- src/main/java/de/atns/common/security/NotInRoleException.java
+++ src/main/java/de/atns/common/security/NotInRoleException.java
@@ -1,5 +1,7 @@
 package de.atns.common.security;
 
+import com.google.gwt.user.client.rpc.IsSerializable;
+
 import java.util.List;
 
 import static java.util.Arrays.asList;
@@ -8,16 +10,19 @@ import static java.util.Arrays.asList;
  * @author tbaum
  * @since 27.11.2009
  */
-public class NotInRoleException extends SecurityException {
+public class NotInRoleException extends SecurityException implements IsSerializable {
 // ------------------------------ FIELDS ------------------------------
 
-    private final List<String> roles;
+    private  List<String> roles;
 
 // --------------------------- CONSTRUCTORS ---------------------------
 
     public NotInRoleException(final Secured secured, final String methodName) {
         super("invalid role to access " + methodName);
         this.roles = asList(secured.value());
+    }
+
+    public NotInRoleException() {
     }
 
 // --------------------- GETTER / SETTER METHODS ---------------------
