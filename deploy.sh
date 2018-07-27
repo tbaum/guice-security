058e061a02f4eb5799efcee8e8b79e84c1344f6a
Thomas Baum <thomas.baum@atns.de>
2018-07-27 13:17:10 +0200

    1.0.7

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+# mvn -DperformRelease=true clean deploy -DaltDeploymentRepository=snapshots::default::https://mvn.atns.de/repository/snapshots
+mvn -DperformRelease=true clean deploy -DaltDeploymentRepository=snapshots::default::https://mvn.atns.de/repository/internal
+
+
